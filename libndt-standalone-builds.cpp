167cf329c39ef7ca640576e27de86f89cd992fdd
Stephen Soltesz <stephen.soltesz@gmail.com>
2023-12-14 15:14:22 -0500

    Remove single includes

--- libndt-standalone-builds.cpp
+++
@@ -1,9 +0,0 @@
-// The purpose of this file is to make sure the standalone
-// header is building, so we include it directly.
-#include "single_include/libndt.hpp"
-int main() {
-  measurement_kit::libndt::Client client{
-    measurement_kit::libndt::Settings{}
-  };
-  client.run();
-}
