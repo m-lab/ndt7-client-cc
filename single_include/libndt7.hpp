dfe881e91c24713c147b917adedfac860e170b07
Stephen Soltesz <stephen.soltesz@gmail.com>
2024-01-08 09:52:20 -0500

    fix(typo): remove double seven from library rename (#22)
    This change fixes a typo remaining from the library renaming.
    
    Fixes: https://github.com/m-lab/ndt7-client-cc/issues/20

--- single_include/libndt7.hpp
+++ single_include/libndt7.hpp
@@ -3,8 +3,8 @@
 
 // libndt7/preamble.hpp - preamble for single include header
 
-#ifndef LIBNDT77_SINGLE_INCLUDE
-#define LIBNDT77_SINGLE_INCLUDE 1
+#ifndef LIBNDT7_SINGLE_INCLUDE
+#define LIBNDT7_SINGLE_INCLUDE 1
 #endif
 
 #endif  // MEASUREMENTLAB_LIBNDT7_PREAMBLE_HPP
